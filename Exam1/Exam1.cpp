f31cfc6ee394f2cb7ab71510e8a343bba9174632
unknown <826096498@qq.com>
2018-11-02 17:02:54 +0800

    ingnore

---
+++ Exam1/Exam1.cpp
@@ -0,0 +1,14 @@
+// Exam1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"      //引用相对路径
+#include <iostream>      //引用绝对路径
+using namespace std;     //全局变量的使用
+
+void main()
+{
+	cout << "  * " << endl
+		<< " * * " << endl
+		<< "* * *" << endl;
+
+}
